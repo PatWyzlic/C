94d0e72286ade667ac79d1310007c9017c444d35
patwyzlic <patrickwyzlic@gmail.com>
2022-09-02 15:18:27 -0400

    Single and multi line comments

---
+++ C/comments/comments.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+
+int main(){
+    //Print statement
+    printf("Hello, \nthis is C\n");
+
+    puts("Hello");
+
+    /* Multi
+    * Line
+    * Comment
+    */
+
+    return(0);
+}
